99dea1dba49b836cf97de5fd4efb6b331df60dd4
PrinOrange <prinorange@outlook.com>
2025-02-27 23:13:33 +0800

    fix: remove unused export.

--- src/plugin.ts
+++ src/plugin.ts
@@ -10,7 +10,7 @@ interface RehypeGraphvizDiagramOption {
   postProcess: (svg: string) => string;
 }
 
-export const defaultOptions: RehypeGraphvizDiagramOption = {
+const defaultOptions: RehypeGraphvizDiagramOption = {
   containerTagName: 'figure',
   containerTagProps: {},
   postProcess: (svg: string) => svg,
